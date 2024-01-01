b41ae889cba1dbbdd4521cd922d24ef0d76a4869
athulkrishnama <athulkrishnama24@gmail.com>
2024-01-01 13:34:14 +0530

    project cleaned up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [toDos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('')
+  return (
+    <div className="App">
+
+      </div>
+  );
+}
+
+export default App;
